9aa8e249fc10d0f49e09f45fd4efb3a9647f2801
Randy Caldejon <rcaldejon@liveaction.com>
2024-04-16 15:58:58 -0400

    Added submodule

---
+++ include/fixbuf/config.h
@@ -0,0 +1,122 @@
+/* include/fixbuf/config.h.  Generated from config.h.in by configure.  */
+/* include/fixbuf/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 to enable aborts of defaulted spec lengths */
+#define FB_ABORT_ON_DEFAULTED_LENGTH 1
+
+/* Define to 1 to enable SCTP support */
+/* #undef FB_ENABLE_SCTP */
+
+/* Define to 1 to use sctp.h header */
+/* #undef FB_INCLUDE_SCTP_H */
+
+/* [Define to 1 to use sctp_uio.h header] */
+/* #undef FB_INCLUDE_SCTP_UIO_H */
+
+/* Define if pointers to integers require aligned access */
+/* #undef HAVE_ALIGNED_ACCESS_REQUIRED */
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the declaration of `ntohll', and to 0 if you don't.
+   */
+#define HAVE_DECL_NTOHLL 0
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the `getaddrinfo' function. */
+#define HAVE_GETADDRINFO 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 to enable OpenSSL support */
+/* #undef HAVE_OPENSSL */
+
+/* Define to 1 to enable DTLS support */
+/* #undef HAVE_OPENSSL_DTLS */
+
+/* Define to 1 if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/errno.h> header file. */
+#define HAVE_SYS_ERRNO_H 1
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libfixbuf"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "netsa-help@cert.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libfixbuf"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libfixbuf 3.0.0.alpha2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libfixbuf"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://tools.netsa.cert.org/fixbuf/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.0.0.alpha2"
+
+/* The size of `double', as computed by sizeof. */
+#define SIZEOF_DOUBLE 8
+
+/* The size of `float', as computed by sizeof. */
+#define SIZEOF_FLOAT 4
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "3.0.0.alpha2"
+
+/* Define if using the dmalloc debugging malloc package */
+/* #undef WITH_DMALLOC */
---
+++ include/fixbuf/version.h
@@ -0,0 +1,60 @@
+/*
+ *  Copyright 2015-2023 Carnegie Mellon University
+ *  See license information in LICENSE.txt.
+ */
+/**
+ *  @file version.h
+ *  Fixbuf's version information
+ *
+ */
+#ifndef _FB_VERSION_H_
+#define _FB_VERSION_H_
+
+/** The first number of libfixbuf's version number. */
+#define FIXBUF_VERSION_MAJOR 3
+/** The second number of libfixbuf's version number. */
+#define FIXBUF_VERSION_MINOR 0
+/** The third number of libfixbuf's version number. */
+#define FIXBUF_VERSION_RELEASE 0
+/** The fourth number of libfixbuf's version number or zero if none. */
+#define FIXBUF_VERSION_BUILD 0
+
+#endif /* _FB_VERSION_H_ */
+
+/*
+ *  @DISTRIBUTION_STATEMENT_BEGIN@
+ *  libfixbuf 3.0.0
+ *
+ *  Copyright 2022 Carnegie Mellon University.
+ *
+ *  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING
+ *  INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON
+ *  UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+ *  AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR
+ *  PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF
+ *  THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF
+ *  ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT
+ *  INFRINGEMENT.
+ *
+ *  Released under a GNU GPL 2.0-style license, please see LICENSE.txt or
+ *  contact permission@sei.cmu.edu for full terms.
+ *
+ *  [DISTRIBUTION STATEMENT A] This material has been approved for public
+ *  release and unlimited distribution.  Please see Copyright notice for
+ *  non-US Government use and distribution.
+ *
+ *  Carnegie Mellon(R) and CERT(R) are registered in the U.S. Patent and
+ *  Trademark Office by Carnegie Mellon University.
+ *
+ *  This Software includes and/or makes use of the following Third-Party
+ *  Software subject to its own license:
+ *
+ *  1. GLib-2.0 (https://gitlab.gnome.org/GNOME/glib/-/blob/main/COPYING)
+ *     Copyright 1995 GLib-2.0 Team.
+ *
+ *  2. Doxygen (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ *     Copyright 2021 Dimitri van Heesch.
+ *
+ *  DM22-0006
+ *  @DISTRIBUTION_STATEMENT_END@
+ */
